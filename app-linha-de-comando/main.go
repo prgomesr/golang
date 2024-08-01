8bc8a603a2b620acd2e15556fbe21d9ffd252ed9
Paulo Gomes <prgomesr@gmail.com>
2024-08-01 14:52:03 -0300

    first commit

---
+++ app-linha-de-comando/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	aplicacao := app.Gerar()
+	if err := aplicacao.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
